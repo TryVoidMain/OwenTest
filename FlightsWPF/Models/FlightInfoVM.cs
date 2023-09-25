08b6dcd50985e8cbce702e5a3fc472ff83baa4f3
Konstantin Shcherbinin <shcherbinin.k@tecomgroup.ru>
2023-09-25 22:53:17 +0300

    Test data template on main window

---
+++ FlightsWPF/Models/FlightInfoVM.cs
@@ -0,0 +1,22 @@
+﻿using Common.Types;
+using System.Collections.Generic;
+using System;
+
+namespace FlightsWPF.Models
+{
+    public class FlightInfoVM
+    {
+        public Guid Id { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public string FlightNumber { get; set; }
+        public List<Passenger> Passengers { get; set; }
+        public FlightInfoVM() { }
+        public FlightInfoVM(DateTime departureTime, string flightNumber, List<Passenger> passengers)
+        {
+            Id = Guid.NewGuid();
+            DepartureTime = departureTime;
+            FlightNumber = flightNumber;
+            Passengers = passengers;
+        }
+    }
+}
--- FlightsWPF/ViewModels/MainWindowVM.cs
+++ FlightsWPF/ViewModels/MainWindowVM.cs
@@ -1,4 +1,10 @@
 ﻿using Common.Services;
+using Common.Types;
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using FlightsWPF.Models;
+using System.Linq;
 
 namespace FlightsWPF.ViewModels
 {
@@ -10,10 +16,42 @@ namespace FlightsWPF.ViewModels
             get => GetValue<string>(nameof(Title)); 
             set => SetValue<string>(nameof(Title), value); 
         }
+
+        public ObservableCollection<FlightInfoVM> Flights
+        {
+            get => GetValue<ObservableCollection<FlightInfoVM>>(nameof(Flights));
+            set => SetValue<ObservableCollection<FlightInfoVM>>(nameof(Flights), value);
+        }
+
+        public FlightInfoVM First => Flights.First();
         public MainWindowVM(IAppService appService)
         {
             _appService = appService;
             Title = "Owen Test";
+
+            Flights = new ObservableCollection<FlightInfoVM>()
+            {
+                new FlightInfoVM()
+                {
+                    DepartureTime = DateTime.Now,
+                    FlightNumber = "First flight number",
+                    Passengers = new List<Passenger>()
+                    {
+                        new Passenger("Vasya", "Pupkin", "Vasiliyevich"),
+                        new Passenger("Akakii", "Petrov", "Petrovich")
+                    }
+                },
+                new FlightInfoVM()
+                {
+                    DepartureTime = DateTime.Now,
+                    FlightNumber = "Second flight number",
+                    Passengers = new List<Passenger>()
+                    {
+                        new Passenger("Vasya", "Vasiliev", "Vasiliyevich"),
+                        new Passenger("Petr", "Petrov", "Petrovich")
+                    }
+                }
+            };
         }
     }
 }
