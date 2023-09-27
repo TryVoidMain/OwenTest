f44f696382447014f6037b0c82f49d48691474e0
Konstantin Shcherbinin <shcherbinin.k@tecomgroup.ru>
2023-09-27 23:55:51 +0300

    Class Helpers

---
+++ FlightsWPF/Helpers/Extensions/FlightCastHelper.cs
@@ -0,0 +1,30 @@
+﻿using Common.Types;
+using System.Collections.Generic;
+using FlightsWPF.Models;
+using System.Linq;
+
+namespace FlightsWPF.Helpers.Extensions
+{
+    public static class FlightCastHelper
+    {
+        public static FlightInfoVM CastToFlightVM(this FlightInfo flightInfo)
+        {
+            return new FlightInfoVM(flightInfo);
+        }
+
+        public static FlightInfo CastToFlight(this FlightInfoVM flightInfo)
+        {
+            var passengers = new List<Passenger>();
+
+            foreach (var pass in flightInfo.Passengers)
+                passengers.Add(pass.CastToPassenger());
+
+            return new FlightInfo()
+            {
+                DepartureTime = flightInfo.DepartureTime,
+                FlightNumber = flightInfo.FlightNumber,
+                Passengers = passengers
+            };
+        }
+    }
+}
---
+++ FlightsWPF/Helpers/Extensions/PassengerCastHelper.cs
@@ -0,0 +1,21 @@
+﻿using Common.Types;
+using FlightsWPF.Models;
+
+namespace FlightsWPF.Helpers.Extensions
+{
+    public static class PassengerCastHelper
+    {
+        public static Passenger CastToPassenger(this PassengerVM passengerVM)
+        {
+            return new Passenger(
+                passengerVM.FirstName,
+                passengerVM.LastName,
+                passengerVM.Patronomyc);
+        }
+
+        public static PassengerVM CastToPassengerVM(this Passenger passenger)
+        {
+            return new PassengerVM(passenger);
+        }
+    }
+}
